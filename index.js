a11dbf5eaedc05c09831531140c6ea240f161c53
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2023-09-17 10:39:43 +0700

    connect db and api

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const shortid = require('shortid');
+const mysql = require("mysql2/promise");
+const cors = require('cors');
+const fetch = require('node-fetch');
+const qr = require('qrcode');
+const { createReadStream } = require('fs');
+const { parse } = require('node-html-parser');
+app.use(express.json());
+app.use(cors());
+
+
+const pool = mysql.createPool({
+    host: 'buuuikfhqw5l0acqlcy2-mysql.services.clever-cloud.com',
+    user: 'uqjnic7dnnlbtjgq',
+    password: 'jfD5NPBmRX4tPwA6VTt2',
+    database: 'buuuikfhqw5l0acqlcy2',
+    waitForConnections: true
+});
+console.log('Connected to MySQL database');
+
+pool.on('error', (err) => {
+    console.error('MySQL pool error:', err);
+});
+
+app.get('/users', async (req, res) => {
+    try {
+        const connection = await pool.getConnection();
+        const [rows, fields] = await connection.query('SELECT * FROM users');
+        connection.release();
+
+        return res.status(200).json(rows);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            code: 500,
+            message: 'Internal server error',
+        });
+    }
+});
 
 app.get('/hello', (req, res) => {
     res.status(200).json({
